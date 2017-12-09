9b39bd8780401a8fe007379b78b45dce9c10b2a8
unknown <tacrawford91@gmail.com>
2017-12-09 11:59:18 -0600

    Broken Code

--- lotto.js
+++ lotto.js
@@ -3,11 +3,15 @@
 // functions 
 function generate() {
     var randomNumber = Math.floor(Math.random()*9999999);
+    return randomNumber;
 }
 
 $("button").on("click", function(event){
     var newNumber = generate();
-    $(".numbers").html(`<h1>${newNumber}</h1>`);
+    var newH1 = $("<h1></h1>")
+    $("newH1").text(newNumber);
+    $(".numbers").append(newH1);
+
 })
 //gerate random number 7 
 //Display number
