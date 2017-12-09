3fbb1220b16baff8e4cf69335c7859d867728341
Zach Johnson <zachjohnson@Zachs-MacBook-Air.local>
2017-12-09 11:56:24 -0600

    practice

--- lotto.js
+++ lotto.js
@@ -3,10 +3,14 @@
 // functions 
 function generate() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     var randomNumber = Math.floor(Math.random()*9999999);
 =======
     var randomNumber = Math.floor(Math.random()*999);
 >>>>>>> e9f673255cff6f5c7b081b5c482861a1bd113a9d
+=======
+    var randomNumber = Math.floor(Math.random()*999);
+>>>>>>> 3f78332eebed2a2d4ed249c0d6d0d703542857a1
     return randomNumber;
 }
 
