8598435de311c91f4bfdebe2cb5b7bebada992f2
Zach Johnson <zachjohnson@Zachs-MacBook-Air.local>
2017-12-09 11:52:20 -0600

    Added jquery cdn. Added return randomNumber.

--- lotto.js
+++ lotto.js
@@ -2,7 +2,11 @@
 
 // functions 
 function generate() {
+<<<<<<< HEAD
     var randomNumber = Math.floor(Math.random()*9999999);
+=======
+    var randomNumber = Math.floor(Math.random()*999);
+>>>>>>> e9f673255cff6f5c7b081b5c482861a1bd113a9d
     return randomNumber;
 }
 
